5deb07904a7238cd4c7499852ef99ce5b6a58748
josh2kv <josh2kv@gmail.com>
2022-06-12 19:26:35 +0900

    9 fix views

--- 9_nodebird(2)/routes/page.js
+++ 9_nodebird(2)/routes/page.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 router.use((req, res, next) => {
-  res.locals.user = null;
+  res.locals.user = req.user;
   res.locals.followerCount = 0;
   res.locals.followingCount = 0;
   res.locals.followerList = [];
   next();
 });
 
-router.get('/profile', (req, res) => {
+// req.isAuthenticated()가 true여야 next가 호출되어 res.render가 있는 미들웨어로 넘어갈 수 있음
+router.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile', { title: '내정보 - NodeBird' });
 });
 
-router.get('/join', (req, res) => {
+router.get('/join', isNotLoggedIn, (req, res) => {
   res.render('join', { title: '회원가입 = NodeBird' });
 });
 
