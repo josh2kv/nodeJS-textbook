fe14f51c9d3bef2cf17b5193cd9a33f6fc8808ea
josh2kv <josh2kv@gmail.com>
2022-06-14 00:20:39 +0900

    9 add follow and hashtag

---
+++ 9_nodebird(2)/routes/user.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const { isLoggedIn } = require('./middlewares');
+const { User } = require('../models');
+
+const router = express.Router();
+
+router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } });
+    console.log('🚀 ~ user', user);
+    console.log('🚀 ~ req', req);
+
+    if (user) {
+      await user.addFollowing(parseInt(req.params.id, 10));
+      res.send('success');
+    } else {
+      res.status(404).send('no user');
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
